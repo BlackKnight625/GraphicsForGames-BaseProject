e36fd612fa696f47ba996153cbcb8fe908700117
ist1102174 <joaopomarto@tecnico.ulisboa.pt>
2022-11-29 14:27:49 +0000

    added threshold

--- Exercises/MathChallenge/main.cpp
+++ Exercises/MathChallenge/main.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+const float THRESHOLD = (float)1.0e-5;
+#define HEADER(X) std::cout << std::endl << (X) << std::endl << std::endl;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
+}
+
+void exercise3() {
+
 }
