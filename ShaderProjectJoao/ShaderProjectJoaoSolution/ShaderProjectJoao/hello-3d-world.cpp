d74b0c698d5470bd2dacad25a3d858992dc53fc6
JoaoPMarto <joaopomarto@tecnico.ulisboa.pt>
2022-12-27 20:29:39 +0000

    added things for second point in proposal

--- ShaderProjectJoao/ShaderProjectJoaoSolution/ShaderProjectJoao/hello-3d-world.cpp
+++ ShaderProjectJoao/ShaderProjectJoaoSolution/ShaderProjectJoao/hello-3d-world.cpp
@@ -26,7 +26,12 @@
 #include "mgl/Mesh.hpp"
 #include "mgl/Cube.hpp"
 #include "mgl/Sphere.hpp"
+#include <glm/gtx/quaternion.hpp>
 
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "../stb/stb_image_write.h"
+
+using namespace std;
 ////////////////////////////////////////////////////////////////////////// MYAPP
 
 class MyApp : public mgl::App {
@@ -39,6 +44,12 @@ private:
 	mgl::MeshManager *MeshManager = nullptr;
     mgl::Cube cube = mgl::Cube();
     mgl::Sphere sphere = mgl::Sphere();
+    bool isPerspective = true;
+    float lastX;
+    float lastY;
+    bool firstMouse = true;
+    float yaw;
+    float pitch;
 
 	void createMeshManager();
 	void createShaderProgram();
@@ -53,6 +64,7 @@ public:
     void displayCallback(GLFWwindow* win, double elapsed) override;
     void windowCloseCallback(GLFWwindow* win) override;
     void windowSizeCallback(GLFWwindow* win, int width, int height) override;
+    void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) override;
     void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) override;
 
     virtual ~MyApp() {
@@ -145,7 +157,7 @@ void MyApp::drawScene() {
 
 ////////////////////////////////////////////////////////////////////// CALLBACKS
 
-void MyApp::initCallback(GLFWwindow *win) {
+void MyApp::initCallback(GLFWwindow* win) {
     createMeshManager();
     createBufferObjects();
     createShaderProgram();
@@ -153,20 +165,16 @@ void MyApp::initCallback(GLFWwindow *win) {
     createSphere();
 }
 
-void MyApp::windowCloseCallback(GLFWwindow *win) { destroyBufferObjects(); }
+void MyApp::windowCloseCallback(GLFWwindow* win) { destroyBufferObjects(); }
 
-void MyApp::windowSizeCallback(GLFWwindow *win, int winx, int winy) {
-  glViewport(0, 0, winx, winy);
+void MyApp::windowSizeCallback(GLFWwindow* win, int winx, int winy) {
+    glViewport(0, 0, winx, winy);
 }
 
-void MyApp::displayCallback(GLFWwindow *win, double elapsed) { drawScene(); }
-
-void MyApp::keyCallback(GLFWwindow* window, int key, int scancode,
-    int action, int mods) {
-
-}
+void MyApp::displayCallback(GLFWwindow* win, double elapsed) { drawScene(); }
 
 const int numberOfPixels = 800 * 600 * 3;
+
 void saveScreenshotToFile(std::string filename, int windowWidth, int windowHeight) {
 
     unsigned char pixels[numberOfPixels];
@@ -184,6 +192,199 @@ void saveScreenshotToFile(std::string filename, int windowWidth, int windowHeigh
     fclose(outputFile);
 
     printf("Finish writing to file.\n");
+}
+
+void MyApp::mouseButtonCallback(GLFWwindow* window, int button, int action, int mods) {
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        if (firstMouse)
+        {
+            lastX = xpos;
+            lastY = ypos;
+            firstMouse = false;
+        }
+        /**
+
+        float xoffset = xpos - lastX;
+        float yoffset = lastY - ypos;
+        lastX = xpos;
+        lastY = ypos;
+
+        float sensitivity = 0.1f;
+        xoffset *= sensitivity;
+        yoffset *= sensitivity;
+
+        yaw += xoffset;
+        pitch += yoffset;
+
+        if (pitch > 89.0f)
+            pitch = 89.0f;
+        if (pitch < -89.0f)
+            pitch = -89.0f;
+
+        glm::vec3 front;
+        front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+        front.y = sin(glm::radians(pitch));
+        front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+
+        glm::mat4 newViewMatrix = glm::lookAt(front, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0,1.0,0.0));
+        Camera->setViewMatrix(newViewMatrix);
+        */
+        float xAngle = lastX - xpos;
+        float yAngle = lastY - ypos;
+
+        glm::quat rotationQX = glm::angleAxis(xAngle, glm::normalize(glm::vec3(0.0, 1.0, 0.0)));
+        glm::mat4 rotationMX = glm::toMat4(rotationQX);
+
+        glm::vec4 eyePositionX();
+
+    }
+}
+
+void flipVertically(int width, int height, char* data)
+{
+    char rgb[3];
+
+    for (int y = 0; y < height / 2; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            int top = (x + y * width) * 3;
+            int bottom = (x + (height - y - 1) * width) * 3;
+
+            memcpy(rgb, data + top, sizeof(rgb));
+            memcpy(data + top, data + bottom, sizeof(rgb));
+            memcpy(data + bottom, rgb, sizeof(rgb));
+        }
+    }
+}
+
+int saveScreenshot(const char* filename)
+{
+    GLint viewport[4];
+    glGetIntegerv(GL_VIEWPORT, viewport);
+
+    int x = viewport[0];
+    int y = viewport[1];
+    int width = viewport[2];
+    int height = viewport[3];
+
+    char* data = (char*)malloc((size_t)(width * height * 3)); // 3 components (R, G, B)
+
+    if (!data)
+        return 0;
+
+    glPixelStorei(GL_PACK_ALIGNMENT, 1);
+    glReadPixels(x, y, width, height, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+    flipVertically(width, height, data);
+
+    int saved = stbi_write_png(filename, width, height, 3, data, 0);
+
+    free(data);
+
+    return saved;
+}
+
+const char* createScreenshotBasename()
+{
+    static char basename[30];
+
+    time_t t = time(NULL);
+    strftime(basename, 30, "%Y%m%d_%H%M%S.png", localtime(&t));
+
+    return basename;
+}
+
+int captureScreenshot()
+{
+    char filename[50];
+
+    strcpy(filename, "screenshots/");
+    strcat(filename, createScreenshotBasename());
+
+    int saved = saveScreenshot(filename);
+
+    if (saved)
+        printf("Successfully Saved Image: %s\n", filename);
+    else
+        fprintf(stderr, "Failed Saving Image: %s\n", filename);
+
+    return saved;
+}
+
+void MyApp::keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+
+    if (glfwGetKey(window, GLFW_KEY_P) == GLFW_PRESS) {
+        //saveScreenshotToFile("screenshot.png", 800, 600);
+        captureScreenshot();
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_L) == GLFW_PRESS) {
+        if (isPerspective) {
+            Camera->setProjectionMatrix(ProjectionMatrix1);
+            isPerspective = false;
+            cout << "\nActivate orthographic projection\n";
+        }
+        else {
+            Camera->setProjectionMatrix(ProjectionMatrix2);
+            isPerspective = true;
+            cout << "\nActivate perspective projection\n";
+        }
+    }
+
+    if (glfwGetKey(window, GLFW_KEY_I) == GLFW_PRESS) {
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    }
+    else if (glfwGetKey(window, GLFW_KEY_O) == GLFW_PRESS) {
+        glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    }
+
+    // Sphere transformations
+    if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS) {
+        sphere.translate(glm::vec3(-1.0f, 0.0f, 0.0f));
+    }
+    if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS) {
+        sphere.translate(glm::vec3(1.0f, 0.0f, 0.0f));
+    }
+    if (glfwGetKey(window, GLFW_KEY_G) == GLFW_PRESS) {
+        sphere.rotate(-15.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+    }
+    if (glfwGetKey(window, GLFW_KEY_H) == GLFW_PRESS) {
+        sphere.rotate(15.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+    }
+    if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS) {
+        sphere.scale(glm::vec3(0.75f, 0.75f, 0.75f));
+    }
+    if (glfwGetKey(window, GLFW_KEY_Y) == GLFW_PRESS) {
+        sphere.translate(glm::vec3(1.0f, 1.0f, 1.0f));
+    }
+
+    // Cube transformations
+    if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS) {
+        cube.translate(glm::vec3(-1.0f, 0.0f, 0.0f));
+    }
+    if (glfwGetKey(window, GLFW_KEY_V) == GLFW_PRESS) {
+        cube.translate(glm::vec3(1.0f, 0.0f, 0.0f));
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        cube.rotate(-15.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+    }
+    if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS) {
+        cube.rotate(15.0f, glm::vec3(0.0f, 0.0f, 1.0f));
+    }
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS) {
+        cube.scale(glm::vec3(0.75f, 0.75f, 0.75f));
+    }
+    if (glfwGetKey(window, GLFW_KEY_R) == GLFW_PRESS) {
+        cube.translate(glm::vec3(1.0f, 1.0f, 1.0f));
+    }
+
+    //Shaders->bind();
+    //glUniform1f(Shaders->Uniforms["Time0_1"].index, k);
+    //Shaders->unbind();
 }
 
 /////////////////////////////////////////////////////////////////////////// MAIN
